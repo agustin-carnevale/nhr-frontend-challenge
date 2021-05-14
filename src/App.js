d72d2327303ef15fb3b8170a268c9a5f7bce3502
Agustin Carnevale <acarnevale@ecointeractive.com>
2021-05-14 14:57:50 -0300

    added fetching logic, loading and error state

--- src/App.js
+++ src/App.js
@@ -1,32 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import AddTenantForm from './components/AddTenantForm';
 import Tabs from './components/Tabs';
 import TenantsTable from './components/TenantsTable';
 import { Service } from './Service';
 
-const mockTenants = [
-  {
-    id: 1,
-    name: "Mark Otto",
-    paymentStatus: "CURRENT",
-    leaseEndDate: "12/31/2020"
-  }
-]
-
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+  const [tenants, setTenants] = useState([]);
+
+  useEffect(() => {
+    fetchTenants();
+  }, []);
+
+  const fetchTenants = async () => {
+    setLoading(true);
+    setError('');
+    setTenants([]);
+    try {
+      const result = await Service.getTenants();
+      setTenants(result);
+    } catch (error) {
+      setError("Something went wrong fetching tenants. Please refresh.");
+    } finally {
+      setLoading(false);
+    }
+  }
 
   return (
-      <>
-        <div className="container">
-          <h1>Tenants</h1>
-          <Tabs />
-          <TenantsTable tenants={mockTenants}/>
-        </div>
-        <div className="container">
-          <button className="btn btn-secondary">Add Tenant</button>
-        </div>
-        <AddTenantForm />
-      </>
+    <>
+      <div className="container">
+        <h1>Tenants</h1>
+        <Tabs />
+        {
+          loading 
+          ? <div>Loading..</div>
+          : <TenantsTable tenants={tenants}/>
+        }
+      </div>
+      <div className="container text-center" style={{color: 'red'}}>
+        { error }
+      </div>
+      <div className="container">
+        <button className="btn btn-secondary">Add Tenant</button>
+      </div>
+      <AddTenantForm />
+    </>
   );
 }
 
